8e29ee6581cfc3167e19648d774da95decd48560
katende ivan <kigh143@gmail>
2016-11-05 02:07:46 +0300

    add the files for the application , the www folder of cordova

---
+++ js/script.js
@@ -0,0 +1,29 @@
+﻿var loginform = $('#login'), signupform = $('#signup') , showSignUpForm = $('#showSignUpForm') , showSignInForm = $('#showSignInForm');
+
+showSignInForm.on('click', function(e){ 
+	loginform.show();
+	signupform.hide();
+});
+
+showSignUpForm.on('click', function(fe){
+	signupform.show();
+	loginform.hide();
+});
+
+
+var createNew = $('#createNew'), applicationForm = $('#applicationForm'), closeForm = $('#closeForm');
+
+closeForm.bind('click', closeApplicationForm);
+createNew.bind('click', showApplicationForm);
+
+function showApplicationForm()
+{
+	applicationForm.show();
+	closeForm.show();
+}
+
+function closeApplicationForm()
+{
+	applicationForm.hide();
+	closeForm.hide();
+}
